182e556d5870e5f28458ba922a3d5766aacd7e73
Milena Kari <milenakari@yahoo.com>
2021-01-18 16:39:11 -1000

    Add GEt from API and post to server

--- website/app.js
+++ website/app.js
@@ -1,11 +1,46 @@
 /* Global Variables */
 
+const API_KEY = '5107f2e7c18d313fdf274b051884c517'
+const baseURL = 'http://api.openweathermap.org/data/2.5/forecast?zip='
+//API CALL
+//http://api.openweathermap.org/data/2.5/forecast?zip=96732&units=imperial&appid=5107f2e7c18d313fdf274b051884c517
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 const data = {temperature: 80, date: newDate, userResponse: "Fake new"}
-const button = document.getElementById("generate");
+const form = document.getElementById('form');
+let res = {}
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const zip = document.getElementById('zip').value;
+    const userFeelings = document.getElementById('feelings').value
+    getWeatherData(baseURL, zip, API_KEY)
+    .then(function(response){
+        console.log("In then ", response.city.name, response.list[0].main.temp, userFeelings)
+        postData('/addData', {temperature: response.list[0].main.temp, date: newDate, userResponse: userFeelings} )
+    })
+}
+
+const getWeatherData = async (baseURL, zip, API_KEY) =>{ 
+    
+    const url = baseURL + zip + '&units=imperial&appid=' + API_KEY;
+    const request = await fetch(url);
+    try {
+    // Transform into JSON
+    const response = await request.json()
+    
+    console.log("Response: ", response.city.name, response.list[0].main.temp )
+    //alert(`response received ${response.list[0].main.temp}`);
+    res=response;
+    return response;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+}
 
 const postData = async(url='', data = {}) => {
     console.log('In postData')
@@ -26,5 +61,5 @@ const postData = async(url='', data = {}) => {
     }
 }
 
-button.addEventListener('click', postData('/addData', data));
+form.addEventListener('submit', handleSubmit);
 //postData('/addData', data)
