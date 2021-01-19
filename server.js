8da3088e086e8e49e804edbb51956ecdc77cc5e8
Milena Kari <milenakari@yahoo.com>
2021-01-19 13:10:01 -1000

    Remove debug comments, adjust width of entryHolder

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-
 // Require Express to run server and routes
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors')
+
 // Start up an instance of app
 const app = express();
 /* Middleware*/
-
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors())
+
 // Cors for cross origin allowance
+const cors = require('cors')
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
-
 
 // Setup Server
 const port = 8000;
@@ -37,5 +36,4 @@ function addData(req, res) {
     projectData.date = req.body.date;
     projectData.userResponse = req.body.userResponse;
     res.send(projectData)
-    console.log("Added data", projectData)
 }
